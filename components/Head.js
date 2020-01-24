ce82688d0926b638eccc0895b5ebd0fd165c4eb5
Justin A <jarata@gmail.com>
2020-01-24 11:06:10 -0800

    fixed console issue with head import NextHead from next

--- components/Head.js
+++ components/Head.js
@@ -1,14 +1,16 @@
-const Head = (props) => {
+import NextHead from 'next/head'
+
+const Header = (props) => {
 	return (
-		<head>
+		<NextHead>
 			<title>{props.title || "Justin Arata - Portfolio"}</title>
 			<meta charSet="UTF-8" />
 			<meta name="author" content="Justin Arata" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta name="description" content={props.description || "Full Stack Web Developer Portfolio"} />
 			<meta name="keywords" content={props.keywords || "HTML, CSS, Less, JavaScript, JS, Next.js, Next, React, React.js, Node, Node.js"} />
-		</head>
+		</NextHead>
 	)
 };
 
-export default Head;
+export default NextHead;
