a4da4523860a9120cb4a17c1db33b9c3ef097588
Justin A <jarata@gmail.com>
2020-02-03 08:44:49 -0800

    imported/exported projects and skills for new endpoints

--- data/index.js
+++ data/index.js
@@ -1,3 +1,7 @@
-import resume from './resume.json'
+import resume from './resume.json';
+import projects from './projects';
+import skills from './skills';
 
 export const Resume = resume;
+export const Projects = projects;
+export const Skills = skills;
