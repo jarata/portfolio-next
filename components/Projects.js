b76802041371bb1344441cc8696309a16eeab0f6
Justin A <jarata@gmail.com>
2020-02-05 16:25:06 -0800

    built out Projects.js styled Projects for mobile

--- components/Projects.js
+++ components/Projects.js
@@ -1,9 +1,26 @@
-const Projects = () => {
+const ProjectCard = (props) => {
 	return (
-		<section>
+		<div className="project-card">
+			<h3>{props.prj.title}</h3>
+			<p>{props.prj.desc}</p>
+			<div>
+				{props.prj.stack.map(stk => (<span key={stk}>{stk}</span>))}
+			</div>
+			<ul>
+				{props.prj.highlights.map(hgl => (<li key={hgl}>{hgl}</li>))}
+			</ul>
+			<a href={props.prj.deployed_url} target="_blank">Deployed Link</a>
+			<a href={props.prj.repo_url} target="_blank">Repo Link</a>
+		</div>
+	)
+};
+
+const Projects = (props) => {
+	return (
+		<section id="projects">
 			<h1>Projects</h1>
 			<div>
-				Projects
+				{props.projects.map(prj => (<ProjectCard key={prj.title} prj={prj}/>))}
 			</div>
 		</section>
 	)
