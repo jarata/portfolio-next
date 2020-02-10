5853ac3d37869b4756623628d1b70fe9dc81354f
Justin A <jarata@gmail.com>
2020-02-10 14:40:22 -0800

    changed server url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://portfolio-next.jarata.now.sh ';
+export const server = dev ? 'http://localhost:3000' : 'https://portfolio-next-llwjaxur4.now.sh';
