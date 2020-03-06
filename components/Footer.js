294da4d15d926f5f090dd6cccabf9b4232ff584c
Justin A <jarata@gmail.com>
2020-03-06 14:58:01 -0800

    refactored Footer.js restyled Footer for desktop

--- components/Footer.js
+++ components/Footer.js
@@ -1,27 +1,7 @@
 const Footer = () => {
 	return (
 		<footer>
-			<div>
-				<a href="https://github.com/jarata/" target="_blank">
-					<img src="/github_logo.png" alt="Github Logo"/>
-				</a>
-				<a href="https://www.linkedin.com/in/justin-arata/" target="_blank">
-					<img src="/linkedin_logo.png" alt="LinkedIn Logo"/>
-				</a>
-			</div>
-			<div>
-				<p>
-					This website was built with
-					<a href="https://zeit.co/" target="_blank">
-						<img src="/nextjs-black-logo.svg" alt="Next.js"/>
-					</a>
-					and deployed with
-					<a href="https://nextjs.org/" target="_blank">
-						<img src="/zeit-black-triangle.svg" alt="Zeit" />
-					</a>
-				</p>
-			</div>
-			<div>
+			<div className="copyright">
 				<p>Copyright © 2018-{new Date().getFullYear()}</p>
 				<p>Justin Arata, All rights reserved.</p>
 			</div>
