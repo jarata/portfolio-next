2a286f15f6b22a3d258a341d629e3ef8ad7a88bc
Justin A <jarata@gmail.com>
2020-03-08 14:37:28 -0700

    refactored About.js restyled About for desktop

--- components/About.js
+++ components/About.js
@@ -4,11 +4,11 @@ const About = () => {
 			<h1>About Me</h1>
 			<p>
 				I can clean your webcode! Let me help your sites load fast, secure, and easier for any engineer to work on your front-end and back-end. As a growth minded developer, I will be responsive to the requirements of projects.
-			</p>
-			<p>
+				<br/>
+				<br/>
 				My current strengths focus on full stack web engineering, but learning new languages is a great opportunity to grow alongside your projects.
-			</p>
-			<p>
+				<br/>
+				<br/>
 				With a background in sales and construction, I am comfortable delivering web product knowledge to roles of all levels. I add value to diverse teams by ensuring visibility and education of newly implemented web tools.
 			</p>
 		</main>
