c2caaad9d51fdadb6496486956c9233281c79be3
Justin A <jarata@gmail.com>
2020-03-08 13:41:12 -0700

    added title to Skills refactored Skills styling for desktop added new skills images added background image updated skills.json with new images

--- components/Skills.js
+++ components/Skills.js
@@ -2,7 +2,8 @@ const SkillCard = (props) => {
 	return (
 		<div className="skill-card">
 			<a href={props.skl.website} target="_blank">
-				<img src={props.skl.logo} alt={`${props.skl.language} Logo`}/>
+				<img src={props.skl.logo} alt={`${props.skl.language} Logo`} />
+				<p>{props.skl.language}</p>
 			</a>
 		</div>
 	)
@@ -12,11 +13,11 @@ const Skills = (props) => {
 	return (
 		<section id="skills">
 			<h1>Skills</h1>
-			<div>
+			<ul>
 				{props.skills.map(skl => (
 					<SkillCard key={skl.language} skl={skl}/>
 				))}
-			</div>
+			</ul>
 		</section>
 	)
 };
